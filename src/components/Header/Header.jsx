e6900ea0d43f6ec18bb9eb214eef1286b84c0d1e
ShahidaAkter1 <shahida0200@gmail.com>
2023-03-31 00:41:13 +0600

    Header section added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const Header = () => {
+    return (
+        <Navbar  expand="lg">
+        <Container>
+          <Navbar.Brand className='fs-2 fw-bold' href="#home">Knowledge Cafe</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto fs-5 fw-semibold">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Component</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+             
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+};
+
+export default Header;
