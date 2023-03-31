57076992d99fc51a110c37201123e7d3c4d2857b
ShahidaAkter1 <shahida0200@gmail.com>
2023-04-01 02:23:17 +0600

    Question Answer all added

--- src/components/QuestionAnswer/QuestionAnswer.jsx
+++ src/components/QuestionAnswer/QuestionAnswer.jsx
@@ -1,61 +1,23 @@
 import React from 'react';
+import './QuestionAnswer.css'
 
 const QuestionAnswer = () => {
     return (
-        <>
-            <div class="accordion my-5 container" id="accordionExample">
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-       Difference between props and state?
-      </button>
-    </h2>
-    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        props are read-only but State changes can be asynchronous.
-        Props allow you to pass data from one component to other components as an argument but State holds information about the components.Props are used to communicate between components but States can be used for rendering dynamic changes with the component.Props make components reusable but State cannot make components reusable.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-       How does useState work?
-      </button>
-    </h2>
-    <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        useState is a one kind of React Hook.It allows user to add state to a functional component.t returns an array with two values: the current state and a function to update it.
+        <div className='container questionAnswer my-5'>
+          <h4>1.Difference between props and state?</h4>
+          <p>** props are read-only but State changes can be asynchronous.
+        Props allow you to pass data from one component to other components as an argument but State holds information about the components.Props are used to communicate between components but States can be used for rendering dynamic changes with the component.Props make components reusable but State cannot make components reusable.</p>
+        <h4>2.How does useState work?</h4>
+        <p>** useState is a one kind of React Hook.It allows the user to add state to a functional component.t returns an array with two values: the current state and a function to update it.
         It works by: Checking if the current index has a value in the state, if not set the default. Then build the setter for the current state index value. Finally increment the index (for the next Hook) and return the setter and current value.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        What else can useEffect() do other than load data?
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-  <div class="accordion-item">
-    <h2 class="accordion-header">
-      <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-        How does React work?
-      </button>
-    </h2>
-    <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
-      <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-      </div>
-    </div>
-  </div>
-</div>
-        </>
+        </p>
+        <h4>3.What else can useEffect() do other than load data?</h4>
+        <p>** Validating an input while it's receiving characters is another great application for useEffect . Whilst the input is being stored in a state using useState , the validation takes place every time the input changes, giving immediate feedback to the user.
+        </p>
+        <h4>4.How does React work?</h4>
+        <p>** The React. js framework is an open-source JavaScript framework and library developed by Facebook. It's used for building interactive user interfaces and web applications quickly and efficiently with significantly less code than you would with vanilla JavaScript.
+</p>
+        </div>
     );
 };
 
